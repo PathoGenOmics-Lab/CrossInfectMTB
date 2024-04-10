fb7d6aac34e7ea98688503c5b3fd0eb1858f9e23
Paula Ruiz Rodriguez <50167687+Paururo@users.noreply.github.com>
2024-04-10 22:04:39 +0200

    Update mapping_steps.sh

--- process_rnaseq-mappings/mapping_steps.sh
+++ process_rnaseq-mappings/mapping_steps.sh
@@ -3,5 +3,5 @@
 fastqc *.fastq.gz
 #  MultiQC sum up
 multiqc .
-# BBDuk for lenght, quality and primer trimming
+# BBDuk for lenght, quality
 bbduk.sh in1=file_1.fastq in2=file_2.fastq out1=file_out_1.fastq out2=file_out_2.fastq outm1=file_out_matched_1.fastq outm2=file_out_matched_2.fastq minlen=35 qtrim=r trimq=35
