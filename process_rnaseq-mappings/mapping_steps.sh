7a9e4858592440f83d68484a2e52efa4836f4136
Paula Ruiz Rodriguez <50167687+Paururo@users.noreply.github.com>
2024-04-10 21:54:33 +0200

    add bbduk

--- process_rnaseq-mappings/mapping_steps.sh
+++ process_rnaseq-mappings/mapping_steps.sh
@@ -1,5 +1,7 @@
-#First evaluate each fastq with FastQC/multiqc
+# First evaluate each fastq with FastQC/multiqc
 # FastQC
 fastqc *.fastq.gz
 #  MultiQC sum up
 multiqc .
+# BBDuk for lenght, quality and primer trimming
+bbduk.sh in1=file_1.fastq in2=file_2.fastq out1=file_out_1.fastq out2=file_out_2.fastq outm1=file_out_matched_1.fastq outm2=file_out_matched_2.fastq ref=adaptadores.fa ktrim=r k=23 mink=11 hdist=1 minlen=35 qtrim=r trimq=35
