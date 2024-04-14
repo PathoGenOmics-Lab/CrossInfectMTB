de0a46ae6b3e8bd61e54d4ba4f8ac3423cc57c5c
Paula Ruiz Rodriguez <50167687+Paururo@users.noreply.github.com>
2024-04-14 10:08:52 +0200

    Update mapping_steps.sh

--- process_rnaseq-mappings/mapping_steps.sh
+++ process_rnaseq-mappings/mapping_steps.sh
@@ -6,7 +6,7 @@ multiqc .
 # BBDuk for lenght, quality
 bbduk.sh in1=file_1.fastq in2=file_2.fastq out1=file_out_1.fastq out2=file_out_2.fastq outm1=file_out_matched_1.fastq outm2=file_out_matched_2.fastq minlen=35 qtrim=r trimq=35
 # Prepare references
-## Transfor annotation gff to gtf (cufflinks)
+## Transform annotation gff to gtf (cufflinks)
 gffread GCF_000001405.26_GRCh38_genomic.gff -T -o GCF_000001405.26_GRCh38_genomic.gtf
 ## Create index with STAR
 STAR --runThreadN 16 \
@@ -15,3 +15,5 @@ STAR --runThreadN 16 \
      --genomeFastaFiles GCF_000001405.26_GRCh38_genomic.fna \
      --sjdbGTFfile GCF_000001405.26_GRCh38_genomic.gtf \
      --sjdbOverhang 100
+## Generate transcripts files for salmon
+gffread -w transcritos_human.fa -g GCF_000001405.26_GRCh38_genomic.fna GCF_000001405.26_GRCh38_genomic.gff
