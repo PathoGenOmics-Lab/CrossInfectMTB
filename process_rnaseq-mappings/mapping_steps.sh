c89acd1ca5f42798a80e0821d39028d9c9962d02
Paula Ruiz Rodriguez <50167687+Paururo@users.noreply.github.com>
2024-04-14 09:01:23 +0200

    add gff to gtf

--- process_rnaseq-mappings/mapping_steps.sh
+++ process_rnaseq-mappings/mapping_steps.sh
@@ -1,7 +1,10 @@
 # First evaluate each fastq with FastQC/multiqc
 # FastQC
 fastqc *.fastq.gz
-#  MultiQC sum up
+# MultiQC sum up
 multiqc .
 # BBDuk for lenght, quality
 bbduk.sh in1=file_1.fastq in2=file_2.fastq out1=file_out_1.fastq out2=file_out_2.fastq outm1=file_out_matched_1.fastq outm2=file_out_matched_2.fastq minlen=35 qtrim=r trimq=35
+# Prepare references
+## Transfor annotation gff to gtf (cufflinks)
+gffread GCF_000001405.26_GRCh38_genomic.gff -T -o GCF_000001405.26_GRCh38_genomic.gtf
